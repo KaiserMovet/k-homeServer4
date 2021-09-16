502f83301921d70ab672b3c83d149f4e0b9af3df
KaiserMovet <rynkiewiczmate@gmail.com>
2021-09-16 14:15:44 +0200

    react

---
+++ start.sh
@@ -0,0 +1,11 @@
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: $0 [python interpreter]"
+  exit 1
+fi
+
+python_interpreter=$1
+echo $python_interpreter
+npm install khS/khS/static --prefix khS/khS/static
+$python_interpreter khS/manage.py migrate
+$python_interpreter khS/manage.py runserver
